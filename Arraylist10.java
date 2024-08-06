53e8b3d1b4e7c47d71002be8f48f13f2c92294ec
Jiya Chowdhury <138868637+jc-2402@users.noreply.github.com>
2024-08-06 23:33:32 +0530

    Add files via upload

---
+++ Arraylist10.java
@@ -0,0 +1,9 @@
+// Question 1 :Monotonic ArrayList(EASY)An Arraylist is monotonic if it is either monotone increasing
+// or monotone decreasing. An Arraylist nums is monotone increasing if for all i<=j, 
+// nums.get(i)<=nums.get(j). An Arraylist nums is monotone decreasing if for all i <= j,
+//  nums.get(i) >= nums.get(j).Given an integer Arraylist nums, return true if the given list is 
+//  monotonic, or false otherwise.
+//  Sample Input 1: nums = [1,2,2,3]
+//  Sample Output 1: true 
+//  Sample Input 2: nums = [6,5,4,4]
+// Sample Output 2: true
---
+++ Arraylist11.java
@@ -0,0 +1,9 @@
+// Question 2 :Lonely Numbers in ArrayList(MEDIUM)You are given an integer arraylist nums.
+// A number x islonely when it appears only once,and no adjacent numbers (i.e. x + 1 and x - 1) 
+// appear in the arraylist. Return all lonely numbers in nums. You may return the answer in any order.
+// Sample Input 1: nums = [10,6,5,8]
+// Sample Output 1: [10,8]
+// Explanation :- 10 is a lonely number since it appears exactly once and 9 and 11 does not appear
+// in nums.- 8 is a lonely number since it appears exactly once and 7 and 9 does not appear in nums.
+// - 5 is not a lonely number since 6 appears in nums and vice versa.
+// Hence, the lonely numbers in nums are [10, 8].Note that [8, 10] may also be returned.
